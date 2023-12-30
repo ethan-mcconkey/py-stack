a271fb50be599c693cabde0f749548e526c11739
ethan-mcconkey <ethanmcconkey@protonmail.com>
2023-12-30 17:24:44 +0200

    feat: Made stack generic

--- py_stack/stack.py
+++ py_stack/stack.py
@@ -1,20 +1,25 @@
-class Stack:
-    def __init__(self, init: list[str]) -> None:
+from typing import TypeVar
+
+
+T = TypeVar("T")
+
+class Stack[T]:
+    def __init__(self, init: list[T] = []) -> None:
         self.stack = init
 
-    def push(self, value: str) -> None:
+    def push(self, value: T) -> None:
         if value == "" or value == None:
             return
 
         self.stack.append(value)
 
-    def pop(self) -> str | None:
+    def pop(self) -> T | None:
         if len(self.stack) == 0:
             return None
 
         return self.stack.pop()
 
-    def peek(self) -> str | None:
+    def peek(self) -> T | None:
         if len(self.stack) == 0:
             return None
         
--- tests/stack_test.py
+++ tests/stack_test.py
@@ -1,34 +1,32 @@
 import pytest
-from py_stack.stack import Stack
+from py_stack.stack import Stack  
 
 def test_stack_initialization():
-    init_values = ["a", "b", "c"]
+    init_values = [1, 2, 3]
     stack = Stack(init_values)
     assert stack.stack == init_values
 
 def test_push():
     stack = Stack([])
-    stack.push("a")
-    assert stack.stack == ["a"]
+    stack.push(4)
+    assert stack.stack == [4]
 
-    stack.push("b")
-    assert stack.stack == ["a", "b"]
+    stack.push(5.5)
+    assert stack.stack == [4, 5.5]
+
+    stack.push("hello")
+    assert stack.stack == [4, 5.5, "hello"]
 
 def test_push_empty_value():
-    stack = Stack(["a", "b"])
-    stack.push("")
-    assert stack.stack == ["a", "b"]
-
-def test_push_none_value():
-    stack = Stack(["a", "b"])
-    stack.push("")
-    assert stack.stack == ["a", "b"]
+    stack = Stack([1, 2, 3])
+    stack.push(None)
+    assert stack.stack == [1, 2, 3]
 
 def test_pop():
-    stack = Stack(["a", "b", "c"])
+    stack = Stack([4, 5, 6])
     popped_value = stack.pop()
-    assert popped_value == "c"
-    assert stack.stack == ["a", "b"]
+    assert popped_value == 6
+    assert stack.stack == [4, 5]
 
 def test_pop_empty_stack():
     stack = Stack([])
@@ -37,10 +35,10 @@ def test_pop_empty_stack():
     assert stack.stack == []
 
 def test_peek():
-    stack = Stack(["x", "y", "z"])
+    stack = Stack([7, 8, 9])
     top_value = stack.peek()
-    assert top_value == "z"
-    assert stack.stack == ["x", "y", "z"]
+    assert top_value == 9
+    assert stack.stack == [7, 8, 9]
 
 def test_peek_empty_stack():
     stack = Stack([])
@@ -48,8 +46,23 @@ def test_peek_empty_stack():
     assert top_value is None
     assert stack.stack == []
 
-# Add more test cases as needed
+def test_push_mixed_types():
+    stack = Stack([])
+    stack.push(1)
+    assert stack.stack == [1]
+
+    stack.push("abc")
+    assert stack.stack == [1, "abc"]
+
+    stack.push(3.14)
+    assert stack.stack == [1, "abc", 3.14]
+
+def test_pop_mixed_types():
+    stack = Stack(["xyz", 42, True])
+    popped_value = stack.pop()
+    assert popped_value is True
+    assert stack.stack == ["xyz", 42]
 
 if __name__ == "__main__":
-    pytest.main()
+    pytest.main(['stack_test.py'])
 
